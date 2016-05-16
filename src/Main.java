585f0e3c6103354eb0c7a984f34f81833e75fdbe
Pete Gordon <peter.gordon87@gmail.com>
2016-05-16 14:35:45 +0100

    Parse first seven values from HITRAN data as doubles.

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,23 @@ public class Main {
     private static File[] directoryListing = dirIn.listFiles();
     private static int gasTally = new File(System.getProperty("user.dir") + "/data/HitranData/").list().length;
 
+    // Values.
+    private static final int KAPPA_LO = 500;
+    private static final int KAPPA_HI = 750;
+    private static final double DELTA_KAPPA = 0.0603/2;
+
 
     public static void main(String[] args) {
-// Process gases.
+        // Process gases.
         if (directoryListing != null) {
             for (File child : directoryListing) {
                 processGas(child);
             }
         }
+    }
+
+    private static double ILNZ(int kappa, double kappa0, double gamma){
+        return 0;
     }
 
     private static void processGas(File inputFile) {
@@ -39,14 +48,22 @@ public class Main {
         List<Double[]> hitranData = new DataFileHelper(inputFile).toStringArray();
 
         // Process.
+        // Integrate each line within bandwidth.
+        // Divide the result by 2*deltakappa to get the average intensity.
+        // Assign result to the midway value kappa+deltakappa.
+
+
 
         // Write to output file.
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(outputFile), "UTF-8"))) {
-            writer.write("derp");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+//                new FileOutputStream(outputFile), "UTF-8"))) {
+//            for (int kappa = KAPPA_LO; kappa <= KAPPA_HI; kappa++){
+//
+//                writer.write(kappa + ", " + result);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
 
@@ -77,15 +94,15 @@ class DataFileHelper {
             if (in != null) {
                 while ((line = in.readLine()) != null) {
                     // Parse line for values.
-                    Double[] values = new Double[4];
+                    Double[] values = new Double[7];
                     line = line.replaceAll("\\s+","");
                     line = line.replaceAll("\\u0000","");
                     Scanner scanner = new Scanner(line);
                     scanner.useDelimiter(",");
-                    for (int i = 0; i < 4; i++){
+                    for (int i = 0; i < 7; i++){
                         values[i] = scanner.nextDouble();
                     }
-                    System.out.println(values[3].toString());
+                    // System.out.println(values[0].toString());
                     // Add line of values to ArrayList.
                     l.add(values);
                 }
